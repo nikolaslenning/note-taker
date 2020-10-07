ffd3fd7335230d45d1e65931c4020c0b70d616d4
Nikolas Lenning <nikolaslenning@gmail.com>
2020-10-07 12:58:49 -0600

    lowercase require express

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,12 @@
 // Dependencies
-var express = require('Express')
+var express = require('express')
 var path = require("path");
 
 // routing
 module.exports = function (app) {
     // https://stackoverflow.com/questions/34105183/uncaught-syntaxerror-unexpected-token-in-nodejs
     // serving static files - another exapmple - https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/Introduction
-    //app.use(express.static('public'));
+    app.use(express.static('public'));
 
     app.get("/notes", function (req, res) {
          res.sendFile(path.join(__dirname, "../public/notes.html"));
