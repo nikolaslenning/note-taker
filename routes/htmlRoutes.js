65bd36ee9843e0ea88b0f661c991d6977badf3a4
Nikolas Lenning <nikolaslenning@gmail.com>
2020-10-07 13:51:06 -0600

    express.static

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,7 +6,7 @@ var path = require("path");
 module.exports = function (app) {
     // https://stackoverflow.com/questions/34105183/uncaught-syntaxerror-unexpected-token-in-nodejs
     // serving static files - another exapmple - https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/Introduction
-    //app.use(express.static('public'));
+    app.use(express.static('public'));
 
     app.get("/notes", function (req, res) {
          res.sendFile(path.join(__dirname, "../public/notes.html"));
